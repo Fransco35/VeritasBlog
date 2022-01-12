4c284fece05ef133f08a85cce9c7e1d28da7a254
Francis Inegbenose <kizzycole10@gmail.com>
2022-01-12 11:20:20 +0000

    put mongo uri in .env folder

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); 
 
-mongoose.connect("mongodb+srv://admin-fransco:admin-fransco@cluster0.vkbie.mongodb.net/newBlog", {useNewUrlParser: true,  useUnifiedTopology: true}); 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,  useUnifiedTopology: true}); 
 
 const userSchema = new mongoose.Schema ({
   email: String,
